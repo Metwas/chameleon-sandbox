e9ed493c6bd490486cd5492e84418160a48a63ed
metwas <metwas.seup@gmail.com>
2024-07-31 17:04:52 +0100

    Updated cloud formation

--- src/scripts/2Dcloud.js
+++ src/scripts/2Dcloud.js
@@ -37,118 +37,133 @@ const { utils, math } = require("broadleaf");
  * @param {CanvasRenderingContext2D} ctx
  * @param {Object} options
  */
-module.exports = function (canvas, ctx, options) {
+module.exports = function (canvas, ctx, options)
+{
 
-    let height = 0;
-    let width = 0;
+       let height = 0;
+       let width = 0;
 
-    let rows = 0;
-    let cols = 0;
-    let resolution = 1;
+       let rows = 0;
+       let cols = 0;
+       let resolution = 12;
 
-    let c_height = 0;
-    let c_width = 0;
+       let c_height = 0;
+       let c_width = 0;
 
-    let fields = [];
-    let yoff = 0;
-    let xoff = 0;
-    let zoff = 0;
-    let angle = 0;
-    let move = 0;
-    let hue = 0;
-    let inc = 0.015;
-    let speed = 0.0001;
+       let fields = [];
+       let yoff = 0;
+       let xoff = 0;
+       let zoff = 0;
+       let angle = 0;
+       let move = 0;
+       let hue = 0;
+       let inc = 0.009;
+       let speed = 0.001;
+       let movement = 0.0003;
 
-    // simplex noise
-    let noise = null;
+       // simplex noise
+       let noise = null;
+       let image = null;
+       let bitmap = null;
 
-    /** Return chameleon sketch template */
-    return {
+       /** Return chameleon sketch template */
+       return {
 
-        /**
-         * Setup entry point
-         *
-         * @param {HTMLCanvasElement} canvas
-         * @param {CanvasRenderingContext2D} ctx
-         * @param {Object} options
-         */
-        setup: function (canvas, options) {
+              /**
+               * Setup entry point
+               *
+               * @param {HTMLCanvasElement} canvas
+               * @param {CanvasRenderingContext2D} ctx
+               * @param {Object} options
+               */
+              setup: function (canvas, options)
+              {
+                     width = canvas.width;
+                     height = canvas.height;
 
-            width = canvas.width;
-            height = canvas.height;
+                     image = new Image();
+                     image.src = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhXJ5iJ3IzveZ6R4opau9Kp0L_L90SpVvRy9egjhzSY-AFysgw6LW3DiEBbv2mbdTtxqgQkbq3_28zKmDYuriZfC-rh3t2qONhPW5wGMSfOYiozw4GfPXyv_NKE53Fe8pT-hgGildZYnlBU/s1600/Capture.JPG";
 
-            // scale rows and cols to set resolution
-            rows = (height / resolution);
-            cols = (width / resolution);
+                     image.onload = function ()
+                     {
+                            bitmap = image;
+                     };
 
-            c_height = resolution;
-            c_width = resolution;
+                     // scale rows and cols to set resolution
+                     rows = (height / resolution);
+                     cols = (width / resolution);
 
-            noise = math.simplex.createNoise();
-            // set noise detail to 8 octaves
-            math.simplex.noiseDetail(8);
+                     c_height = resolution;
+                     c_width = resolution;
 
-            // add canvas mouse-move event listener
-            canvas.onmousemove = function (event) {
-                mouseX = event.offsetX || event.layerX;
-                mouseY = event.offsetY || event.layerY;
-            };
+                     noise = math.simplex.createNoise();
+                     // set noise detail to 8 octaves
+                     math.simplex.noiseDetail(8);
 
-            // create a multidimensional array
-            for (let x = 0; x < cols; x++) {
+                     // add canvas mouse-move event listener
+                     canvas.onmousemove = function (event)
+                     {
+                            mouseX = event.offsetX || event.layerX;
+                            mouseY = event.offsetY || event.layerY;
+                     };
 
-                const k = [];
-                for (let y = 0; y < rows; y++) {
-                    k.push(0);
-                }
+                     // create a multidimensional array
+                     for (let x = 0; x < cols; x++)
+                     {
+                            const k = [];
+                            for (let y = 0; y < rows; y++)
+                            {
+                                   k.push(0);
+                            }
 
-                fields.push(k);
+                            fields.push(k);
+                     }
 
-            }
+              },
 
-        },
+              /**
+               * Main loop
+               *
+               * @param {HTMLCanvasElement} canvas
+               * @param {CanvasRenderingContext2D} ctx
+               * @param {Object} options
+               */
+              loop: function (canvas, ctx, options)
+              {
+                     if (bitmap)
+                     {
+                            ctx.drawImage(bitmap, 0, 0, width, height);
+                     }
 
-        /**
-         * Main loop
-         *
-         * @param {HTMLCanvasElement} canvas
-         * @param {CanvasRenderingContext2D} ctx
-         * @param {Object} options
-         */
-        loop: function (canvas, ctx, options) {
+                     ctx.lineWidth = 1;
 
-            ctx.fillStyle = "steelblue";
-            ctx.fillRect(0, 0, width, height);
+                     move += speed;
+                     xoff = move;
+                     // draw grid
+                     for (let x = 0; x < cols; x++)
+                     {
+                            angle += (0.0003 * x);
 
-            move += speed;
-            xoff = move;
-            // draw grid
-            for (let x = 0; x < cols; x++) {
+                            for (let y = 0; y < rows; y++)
+                            {
+                                   fields[ x ][ y ] = noise(xoff, yoff, zoff);
+                                   let sum = fields[ x ][ y ] * 255;
 
-                angle += (0.0003 * x);
+                                   let alpha = math.map(sum, -15, 20, 0, 0.999);
+                                   sum = sum > -10 ? (sum + 200) : (sum + 500);
+                                   //ctx.fillStyle = "#FFFFFF55";
+                                   ctx.fillStyle = "rgba(" + sum + "," + sum + ", " + sum + "," + alpha + ")";
+                                   ctx.fillRect((x * resolution), (y * resolution), c_width, c_height);
 
-                for (let y = 0; y < rows; y++) {
+                                   yoff += inc;
+                            }
 
-                    fields[x][y] = noise(xoff, yoff, zoff);
+                            xoff += inc;
+                            yoff = 0;
+                     }
 
-                    ctx.lineWidth = resolution;
-                    ctx.fillStyle = "rgb(" + fields[x][y] * 5 + "," + fields[x][y] * 155 + ", " + fields[x][y] * 5 + ")";
-
-                    ctx.fillRect((x * resolution), (y * resolution), c_width, c_height);
-
-                    yoff += inc;
-
-                }
-
-                xoff += inc;
-                yoff = 0;
-
-            }
-
-            zoff += 0.003;
-
-        }
-
-    };
+                     zoff += movement;
+              }
+       };
 
 };
